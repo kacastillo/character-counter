a80a9567e3b041945879db73903089c1cf54bb85
kelley <kelleycastillo@outlook.com>
2026-04-16 11:12:44 -0700

    follow along in class

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,29 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
+
+    @Test
+    void testCharacterShowsUpMultipleTimesGivesCorrectFrequency() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        // Act
+        int actual = counter.getFrequency('l'); // what i want to check
+
+        // Assert
+        assertEquals(2, actual); //expected response, vs actual response
+    }
+
+    @Test
+    void testGetRelativeFrequencyReturns0ForMissingCharacter() {
+        // Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hello");
+
+        // Act
+        double actual = counter.getRelativePercentage('x'); // what i want to check
+
+        // Assert
+        assertEquals(0.0, actual, 0.0001); //expected response, vs actual response
+    }
     
 }
